f71c6684d83be7130dd872ca7cc21c1024dd4f87
Levins <Levinskamaa@gmail.com>
2023-03-10 10:44:59 -0800

    mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,46 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
  * main - entry point
- * Description: adds 2 positive numbers
+ * Description: multiply 2 nums and give result
  * @argc: arguement count
- * @argv: argument array
+ * @argv: arguement array
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int sum;
-	int num;
-	int arg_index;
-	int char_index;
+	int num1;
+	int num2;
+	int result;
 
-	sum = 0;
-	if (argc == 1)
+	if (argc != 3)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	for (arg_index = 1; arg_index < argc; arg_index++)
-	{
-		for (char_index = 0; argv[arg_index][char_index] != '\0'; char_index++)
-		{
-			if (!isdigit(argv[arg_index][char_index]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		num = atoi(argv[arg_index]);
-		if (num < 0)
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += num;
-	}
-	printf("%d\n", sum);
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
 	return (0);
 }
+
